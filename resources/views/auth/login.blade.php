e83bc9ad86333c061554f71eb39fc68980efe9db
JaegerDaFBoi <mcallister2696@gmail.com>
2022-10-09 23:40:00 -0500

    plantilla instalada. login configurado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,57 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    @section('header_contenido')
+    <div class="page-header min-vh-75">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                    <div class="card card-plain mt-8 bg-white">
+                        <div class="card-header pb-0 text-left">
+                            <p class="card-header font-weight-bolder text-info text-gradient">Bienvenido de vuelta!</p>
+                            <p class="mb-0">Confirma tu correo electrónico y contraseña para acceder</p>
+                        </div>
+                        <div class="card-body">
+                            <x-jet-validation-errors class="mb-4" />
+                            @if (session('status'))
+                            <div class="mb-4 font-medium text-sm text-green-600">
+                                {{ session('status') }}
+                            </div>
+                            @endif
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <label for="email">Correo Electrónico</label>
+                                <div class="mb-3">
+                                    <input type="email" name="email" id="email" class="form-control"  aria-label="Email" aria-describedby="email-addon" :value="old('email')" required autofocus />
+                                </div>
+                                <label for="password">Contraseña</label>
+                                <div class="mb-3">
+                                    <input type="password" name="password" id="password" class="form-control" placeholder="Password"  aria-describedby="password-addon" required autocomplete="current-password" />
+                                </div>
+                                <div class="form-check form-switch">
+                                    <input type="checkbox" name="remember_me" id="remember_me" class="form-check-input" checked="">
+                                    <label for="remember_me" class="form-check-label font-weight-bold">Recuérdame</label>
+                                </div>
+                                <div class="text-center ">
+                                    <button type="submit" class="btn btn-round bg-gradient text-red w-100 mt-4 mb-0 font-weight-bolder" style="background-color: #F67B16">Iniciar Sesión</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                            @if (Route::has('password.request'))
+                                <p class="mb-4 text-sm mx-auto">
+                                    ¿Olvidaste tu contraseña?
+                                    <a href="{{ route('password.request') }}" class="text-info text-gradient font-weight-bold">Recuperar Contraseña</a>
+                                </p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                        <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url({{ asset('assets/img/fondomadera.jpg') }})"></div>
+                    </div>
+                </div>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+        </div>
+    </div>
+    @endsection
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,144 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Aldemiti') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+    <!-- Icons -->
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('assets/css/nucleo-svg.css') }}" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <link id="pagestyle" rel="stylesheet" href="{{ asset('assets/css/soft-ui-dashboard.css') }}" />
+
+    {{-- <link rel="stylesheet" href="{{ asset('assets/bootstrap-5.2.2-dist/css/bootstrap.min.css') }}" /> --}}
+
+    <!-- Tempusdominus Bbootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/js/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}" />
+    <!--DataTables -->
+    <link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/js/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
+    @livewireStyles
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="page-background-color">
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg content-orange top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4"
+                    style="border-radius: 60px">
+                    <div class="container-fluid pe-0">
+                        <div class="row">
+                            <div class="col-md-4 d-flex justify-content-start">
+                                <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 justify-items-center"
+                                    href="{{ url('/dashboard') }}">
+                                    <img src="{{ asset('assets/img/avocado.jpg') }}" alt="logoaldemiti"
+                                        class="rounded-circle" width="20%" height="auto">
+                                </a>
+                                <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon mt-2">
+                                        <span class="navbar-toggler-bar bar1"></span>
+                                        <span class="navbar-toggler-bar bar2"></span>
+                                        <span class="navbar-toggler-bar bar3"></span>
+                                    </span>
+                                </button>
+                            </div>
+                            <div class="col-md-4 d-flex justify-content-center flex-wrap align-content-center">
+                                <h2 class="font-weight-bolder display-6 ms-lg-0 ms-3 text-red justify-self-center">
+                                    ALDEMITI</h2>
+                            </div>
+                            <div class="col-md-4 d-flex justify-content-center flex-wrap align-content-center">
+                                <div class="collapse navbar-collapse" id="navigation">
+                                    <ul class="navbar-nav mx-auto ms-xl-auto me-xl-7">
+                                        @if (Route::has('login'))
+                                        @auth
+                                        <li class="nav-item">
+                                            <a class="nav-link me-2 text-red" href="{{ url('/dashboard') }}">
+                                                <i class="ni ni-box-2 opacity-6 text-dark me-1"></i>
+                                                Dashboard
+                                            </a>
+                                        </li>
+                                        @else
+                                        <li class="nav-item">
+                                            <a class="nav-link me-2" href="{{ route('login') }}">
+                                                <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                                                Iniciar Sesión
+                                            </a>
+                                        </li>
+                                        @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link me-2" href="{{ route('register') }}">
+                                                <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                                                Registrarse
+                                            </a>
+                                        </li>
+                                        @endif
+                                        @endauth
+                                        @endif
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
-    </body>
+    </div>
+    <main class="main-content mt-0">
+        @yield('header_contenido')
+        @yield('contenido')
+        @yield('footer_contenido')
+    </main>
+
+
+    <!-- jQuery -->
+    <script src="{{ asset('assets/js/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('assets/js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Theme Core Scripts -->
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+
+    <!-- Theme JS Scripts -->
+    <script src="{{ asset('assets/js/soft-ui-dashboard.min.js') }}"></script>
+    <!-- Bootstrap 5 -->
+    <script src="{{ asset('assets/bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables -->
+    <script src="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <!-- Moment JS -->
+    <script src="{{ asset('assets/js/plugins/moment/moment.min.js') }}"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="{{ asset('assets/js/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}">
+    </script>
+
+    @livewireScripts
+
+    @stack('js')
+</body>
+
 </html>
