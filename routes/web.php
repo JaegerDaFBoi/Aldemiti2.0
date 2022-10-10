794abec6fb564c7da4e63a0f2b4ff5614cbdaece
JaegerDaFBoi <mcallister2696@gmail.com>
2022-10-10 13:35:41 -0500

    rutas creadas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\ProveedorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +27,41 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+
+    Route::prefix('inventario')->group(function () {
+        Route::name('inventario.')->group(function () {
+            Route::controller(ProductoController::class)->group(function () {
+                Route::get('/index', 'index')->name('index');
+                Route::get('/crearproducto', 'create')->name('create');
+                Route::post('/guardar', 'store')->name('store');
+                Route::get('/{producto}/editar', 'edit')->name('edit');
+                Route::delete('/{producto}/eliminar', 'destroy')->name('delete');
+            });
+        });
+    });
+
+    Route::prefix('proveedor')->group(function () {
+        Route::name('proveedor.')->group(function () {
+            Route::controller(ProveedorController::class)->group(function () {
+                Route::get('/index', 'index')->name('index');
+                Route::get('/crearproveedor', 'create')->name('create');
+                Route::get('/{proveedor}/editar', 'edit')->name('edit');
+                Route::delete('/{proveedor}/eliminar', 'destroy')->name('delete');
+            });
+        });
+    });
+
+    Route::get('/ingresos/index', [IngresoController::class, 'index'])->name('ingresos.index');
+
+    Route::get('/retiros/index', [RetiroController::class, 'index'])->name('retiros.index');
+
+    Route::get('/ventas/index', [VentaController::class, 'index'])->name('ventas.index');
+    Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas.create');
 });
